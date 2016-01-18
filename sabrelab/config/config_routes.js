89b7242e79e8295df2f1567b6c3f5524951d6654
Louise <louise.beh@gmail.com>
2016-01-18 16:37:19 +0000

    set up api requests

--- sabrelab/config/config_routes.js
+++ sabrelab/config/config_routes.js
@@ -8,6 +8,20 @@ var sabre_dev_studio = new SabreDevStudio({
 });
 var options = {};
 
+module.exports = function(app) {
+
+  app.get('/api/v1/cities', function(req,res) {
+    sabreCall('/v1/lists/supported/cities', res);
+  });
+
+  app.get('/api/v1/places', function(req,res) {
+    sabreCall('/v1/shop/flights/fares?origin=' + req.query.origin +
+    '&departuredate=' + req.query.departuredate +
+    '&returndate=' + req.query.returndate +
+    '&maxfare=' + req.query.maxfare, res);
+  });
+};
+
 function sabreCall(q, res) {
   sabreDevStudio.get(q, options, function(err, data) {
     response(res, err, data);
