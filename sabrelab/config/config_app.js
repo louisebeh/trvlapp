1e2bc538b703944a4d9b1f458a5169a43dbd95fb
Louise <louise.beh@gmail.com>
2016-01-18 16:03:32 +0000

    Set up server

--- sabrelab/config/config_app.js
+++ sabrelab/config/config_app.js
@@ -16,3 +16,18 @@ module.exports = function(app) {
   app.use(cookieParser('sabreSessions'));
 
   app.use(methodOverride());
+
+  app.use(express.static(__dirname + '/../www/'));
+  app.all("/api/*", function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, Accept");
+      res.header("Access-Control-Allow-Methods", "GET, PUT, POST, HEAD, DELETE, OPTIONS");
+      return next();
+  });
+
+  app.use(errorhandler({
+      dumpExceptions: true,
+      showStack: true
+  }));
+
+};
